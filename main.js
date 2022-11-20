671eab9044ba214cf377c4be7899ad0f4836a374
besim <besim@pop-os.localdomain>
2022-11-20 22:23:56 +0300

    ney

---
+++ main.js
@@ -0,0 +1,32 @@
+
+let dict={
+    "hız" : "x=vt",
+    "Newton 1":"F=ma",
+}
+
+kutu = document.getElementById("kutu");
+
+function Ara(){
+    val = (kutu.value)
+    leybıl = document.getElementById("leybıl")
+    if (leybıl){
+        document.body.removeChild(leybıl)
+        }
+    if (val in dict){
+        leybıl = document.createElement("div")
+        leybıl.id = "leybıl"
+        document.body.appendChild(leybıl)
+        console.log(kutu.value)
+        kutu.placeholder = "You Can Search Here!"
+        kutu.value = ""
+        leybıl.innerHTML = `${dict.$val}`
+    }
+    else{
+        kutu.value = ""
+        kutu.placeholder = "Tekrar Dene"
+    }
+}
+
+button = document.getElementById("tus");
+button.addEventListener("click",Ara());
+kutu.placeholder = "You Can Search Here!"
