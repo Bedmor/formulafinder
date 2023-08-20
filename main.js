0e128fc4d39974531944848e641d67edf9fd5149
Bedmor <besimozdemir33@gmail.com>
2023-08-20 03:24:22 +0300

    minor changes(changed everything)

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ let dict={
     "logab":"logac/logbc",
 }
 
-kutu = document.getElementById("kutu");
+let kutu = document.getElementById("kutu");
 
 function Ekle(){
-    fetch("http://localhost:80",{
+    response = fetch("http://localhost:80",{
         method:"POST",
-        body:`${kutu.value} : ${formul_ad = prompt("Enter desired formula")}`,
+        body:`{
+            "${kutu.value}" : "${formul_ad = prompt("Enter desired formula")}"
+        }`,
+        headers:{
+            "Content-Type":'application/json',
+        },
     })
+
+    console.log(response.body)
+    return response
 }
 function Ara(){
     val = (kutu.value).toLowerCase();
@@ -19,23 +27,31 @@ function Ara(){
     if (leybıl){
         document.body.removeChild(leybıl)
         }
-    if (val in dict){
+    try{
+        response = fetch("http://localhost:80",{
+        method:"POST",
+        body:"{$val}",
+        headers:{
+            "Content-Type":"text/plain"
+       },
+    })
+    if (response != "undefined" || kutu.value == null){
         leybıl = document.createElement("div")
         leybıl.id = "leybıl"
         document.body.appendChild(leybıl)
         console.log(kutu.value)
         kutu.placeholder = "You Can Search Here!"
         kutu.value = ""
-        leybıl.innerHTML = `${dict[val]}`
+        leybıl.innerHTML = `${response.body}`
     }
-    else{
+    }
+    
+    catch{
         kutu.value = ""
-        kutu.placeholder = "Tekrar Dene"
+        kutu.placeholder = "Try to search for someting else"
     }
 }
 
 button = document.getElementById("tus");
-button.addEventListener("click",Ara());
+button.addEventListener("click",() => Ara() );
 kutu.placeholder = "You Can Search Here!"
-
-adder = document.getElementById("add");
--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
 const sqlite = require("sqlite3");
 const express = require("express")
+const cors = require("cors");
+const app = express()
 
-app = express()
+db = new sqlite.Database("database.sqlite",sqlite.OPEN_READWRITE)
+db.run("CREATE TABLE IF NOT EXISTS formulafinder (formul TEXT,formul_ad TEXT,UNIQUE(formul, formul_ad))")
 
-db = new sqlite.Database("db.sqlite",sqlite.OPEN_READWRITE)
-db.run("CREATE TABLE IF NOT EXISTS formulafinder (formul TEXT,formul_ad TEXT)")
-db.close()
+
+app.use(express.json(type="application/json"),cors())
+
+//TODO Find a way to handle multiple post requests
+app.post("/ekle",(req,res)=>{
+    ara = db.get(`select * from formulafinder where formul_ad = ${req.body}`)
+    res.send(ara)
+})
+
+app.post("/",(req,res) => {
+    try{
+        jeyson = req.body
+        console.log(Object.keys(jeyson)[0])
+        db.run(`REPLACE INTO formulafinder VALUES ("${Object.keys(jeyson)[0]}","${Object.values(jeyson)[0]}")`)
+        res.send("Başarıyla eklendi")
+    }
+    catch(err) {
+        res.send(alert(err.message))
+    }
+    
+    
+});
+app.listen(80)
+
